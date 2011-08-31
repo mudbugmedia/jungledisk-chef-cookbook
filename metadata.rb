5430033b8a5a0a9dde9bff5dc6f70c3ff3ed7053
Gabe Martin-Dempesy <gabe@mudbugmedia.com>
2011-08-31 12:36:49 -0500

    Updating metadata.rb for uploading to Opscode Community

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,9 @@ license          "Apache 2.0"
 description      "Installs/Configures jungledisk"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
+supports 'ubuntu'
+recipe "jungledisk::default", "Download and install JungleDiskServer package, and configure the license xml"
+attribute 'jungledisk/serial',
+  :display_name => "Serial Number",
+  :description => "Serial number provided by jungledisk.com control panel",
+  :required => "required"
